c48b3972e5ad2b198b2cb8a8e28d20695f113b3d
Kevin Herrera <kevin@herrera.io>
2017-01-03 13:25:03 -0800

    Adding untracked exception for database manager.

---
+++ src/KHerGe/Excel/Exception/Database/NoSuchColumnException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace KHerGe\Excel\Exception\Database;
+
+use KHerGe\Excel\Exception\DatabaseException;
+
+/**
+ * An exception that is thrown for a column that does not exist.
+ *
+ * @author Kevin Herrera <kevin@herrera.io>
+ */
+class NoSuchColumnException extends DatabaseException
+{
+}
